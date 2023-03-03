78c062cff4ee3ff3ce44cf5a3507161ebd5374c7
vishaltkk <tailwindninja@gmail.com>
2023-03-03 12:58:34 +0530

    reoved the lint issues

--- src/utils/sockets/messageHandler.tsx
+++ src/utils/sockets/messageHandler.tsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (io, socket) => {
   const createdMessage = (msg) => {
     socket.broadcast.emit('newIncomingMessage', msg);
--- src/utils/useNewTabOpener.ts
+++ src/utils/useNewTabOpener.ts
@@ -1,5 +1,4 @@
 const openInNewTab = (url: string) => {
-  console.log(url);
   window.open(url, '_blank', 'noopener,noreferrer');
 };
 export default openInNewTab;
